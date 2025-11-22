4c1df860ab899aec8b8e657fe2879479d9d48fad
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2025-11-22 15:05:06 +0100

    Use streams

--- cu/basic.cu
+++ cu/basic.cu
@@ -9,6 +9,8 @@
 template <std::floating_point FP>
 __global__ void multiply_kernel(FP* ptr, FP factor, std::size_t arr_len,
                                 unsigned int block_threads) {
+    assert(threadIdx.y == 0 and threadIdx.z == 0);
+    assert(blockIdx.y == 0 and blockIdx.z == 0);
     const unsigned int i = threadIdx.x + blockIdx.x * block_threads;
     if (i < arr_len) {
         ptr[i] *= factor;
@@ -22,22 +24,28 @@ int main() {
     std::array<float_pt, c::array_len> arr_h;
     std::ranges::generate(arr_h, [i = 0]() mutable { return i++ / std::sqrt(float_pt{2}); });
 
+    // Streams
+    cudaStream_t stream;
+    check_err(cudaStreamCreate(&stream));
+
     // Memory allocation
     const std::size_t mem_size = arr_h.size() * sizeof(float_pt);
     float_pt* arr_d = nullptr;
-    check_err(cudaMalloc((void**)&arr_d, mem_size));
+    check_err(cudaMallocAsync((void**)&arr_d, mem_size, stream));
 
     // Memory copy
-    check_err(cudaMemcpy(arr_d, arr_h.data(), mem_size, cudaMemcpyHostToDevice));
+    check_err(cudaMemcpyAsync(arr_d, arr_h.data(), mem_size, cudaMemcpyHostToDevice, stream));
 
     // Kernel launch
     const work_division work_div = make_work_div(arr_h.size());
-    multiply_kernel<float_pt><<<work_div.blocks, work_div.block_threads>>>(
+    multiply_kernel<float_pt><<<work_div.blocks, work_div.block_threads, 0, stream>>>(
         arr_d, 2, arr_h.size(), work_div.block_threads.x);
 
-    check_err(cudaMemcpy(arr_h.data(), arr_d, mem_size, cudaMemcpyDeviceToHost));
-    check(std::span(arr_h), [](int i) { return i * std::sqrt(float_pt{2}); });
+    check_err(cudaMemcpyAsync(arr_h.data(), arr_d, mem_size, cudaMemcpyDeviceToHost, stream));
 
     // Memory deallocation
-    check_err(cudaFree(arr_d));
+    check_err(cudaFreeAsync(arr_d, stream));
+
+    check_err(cudaStreamSynchronize(stream));
+    check(std::span(arr_h), [](int i) { return i * std::sqrt(float_pt{2}); });
 }
