7ffb82626074a949112b576d9044a8b035589d25
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2025-11-22 15:06:44 +0100

    Rewordings

--- cu/basic.cu
+++ cu/basic.cu
@@ -28,22 +28,22 @@ int main() {
     cudaStream_t stream;
     check_err(cudaStreamCreate(&stream));
 
-    // Memory allocation
+    // Allocation
     const std::size_t mem_size = arr_h.size() * sizeof(float_pt);
     float_pt* arr_d = nullptr;
     check_err(cudaMallocAsync((void**)&arr_d, mem_size, stream));
 
-    // Memory copy
+    // Copy
     check_err(cudaMemcpyAsync(arr_d, arr_h.data(), mem_size, cudaMemcpyHostToDevice, stream));
 
-    // Kernel launch
+    // Kernels
     const work_division work_div = make_work_div(arr_h.size());
     multiply_kernel<float_pt><<<work_div.blocks, work_div.block_threads, 0, stream>>>(
         arr_d, 2, arr_h.size(), work_div.block_threads.x);
 
     check_err(cudaMemcpyAsync(arr_h.data(), arr_d, mem_size, cudaMemcpyDeviceToHost, stream));
 
-    // Memory deallocation
+    // Deallocation
     check_err(cudaFreeAsync(arr_d, stream));
 
     check_err(cudaStreamSynchronize(stream));
