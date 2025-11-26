b7c883eb8f981f4f847b84ef99ca94960201ab31
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2025-11-26 19:34:07 +0100

    Add function for comparison of ranges

--- hpp/check.hpp
+++ hpp/check.hpp
@@ -7,7 +7,9 @@
 #include <cassert>
 #include <cuda.h>
 #include <ranges>
+#include <span>
 #include <utility>
+#include <vector>
 
 namespace cuda_tutorial {
 
@@ -36,14 +38,35 @@ bool are_equal(T const& t1, T const& t2) {
 } // namespace internal
 
 // TODO: Extend to ranges (to by taken by const& instead of value)
+template <std::ranges::view View1, std::ranges::view View2>
+    requires requires {
+        typename std::common_type_t<std::ranges::range_value_t<View1>,
+                                    std::ranges::range_value_t<View1>>;
+    }
+void check(View1 view1, View2 view2) {
+    assert(view1.size() == view2.size());
+    using Elem1 = std::ranges::range_value_t<View1>;
+    using Elem2 = std::ranges::range_value_t<View2>;
+    using Elem = std::common_type_t<Elem1, Elem2>;
+
+    // TODO: In c++23, use std::ranges::all_of with std::views::zip
+    std::vector<Elem> equalities(view1.size());
+    std::ranges::transform(view1, view2, equalities.begin(), [](Elem1 const& e1, Elem2 const& e2) {
+        return internal::are_equal(e1, e2);
+    });
+    assert(std::ranges::all_of(equalities, std::identity()));
+}
+
 template <std::ranges::view View, concepts::checker<std::ranges::range_value_t<View>> Checker>
 void check(View view, Checker&& checker) {
     using Elem = std::ranges::range_value_t<View>;
 
     // TODO: In c++23, use std::views::enumerate
-    assert(std::ranges::all_of(view, [&c = std::as_const(checker), i = 0](Elem const& x) mutable {
-        return internal::are_equal(x, static_cast<Elem>(c(i++)));
-    }));
+    std::vector<Elem> to_check(view.size());
+    std::ranges::generate(to_check, [&c = std::as_const(checker), i = 0]() mutable {
+        return static_cast<Elem>(c(i++));
+    });
+    check(view, std::span(to_check));
 }
 
 } // namespace cuda_tutorial
