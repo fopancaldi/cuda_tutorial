346e30d24dd3c64190b11f84a16d44f7a03b7df3
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2025-12-04 14:24:38 +0100

    Add template in make_work_div

--- hpp/concepts.hpp
+++ hpp/concepts.hpp
@@ -1,11 +1,22 @@
 #pragma once
 
+#include <concepts>
 #include <type_traits>
 
 namespace cuda_tutorial::concepts {
 
 template <typename T>
 concept arithmetic = std::is_arithmetic_v<T>;
+
+template <typename T>
+concept integral_of_int_ref = std::integral<std::remove_reference_t<T>>;
+
+template <typename T>
+concept unsigned3 = requires(T t) {
+    { t.x } -> integral_of_int_ref;
+    { t.y } -> integral_of_int_ref;
+    { t.z } -> integral_of_int_ref;
+};
 
 template <typename Checker, typename T>
 concept checker = requires(Checker c, int i) {
--- hpp/work_division.hpp
+++ hpp/work_division.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
+#include "concepts.hpp"
 #include "constants.hpp"
 #include "typedefs.hpp"
 
 #include <cassert>
 #include <concepts>
 #include <limits>
+#include <utility>
 
 namespace cuda_tutorial {
 
@@ -17,13 +19,15 @@ auto ratio_rounded_up(std::integral auto num, std::integral auto den) {
 
 } // namespace internal
 
-inline work_division make_work_div(ulonglong3 elements) {
+template <concepts::unsigned3 Unsigned3>
+inline work_division make_work_div(Unsigned3 elements) {
     namespace c = constants;
     namespace i = internal;
+    using dim3_val = decltype(std::declval<dim3>().x);
 
-    const auto cast = [](unsigned long long u) {
-        assert(u <= std::numeric_limits<unsigned int>::max());
-        return static_cast<unsigned int>(u);
+    const auto cast = [](auto u) {
+        assert(u <= std::numeric_limits<dim3_val>::max());
+        return static_cast<dim3_val>(u);
     };
     const dim3 blocks{cast(i::ratio_rounded_up(elements.x, c::block_threads.x)),
                       cast(i::ratio_rounded_up(elements.y, c::block_threads.y)),
@@ -32,7 +36,8 @@ inline work_division make_work_div(ulonglong3 elements) {
     return {blocks, c::block_threads};
 }
 
-inline work_division make_work_div(std::size_t elements) {
+template <std::unsigned_integral U>
+inline work_division make_work_div(U elements) {
     return make_work_div(ulonglong3{elements, 1, 1});
 }
 
