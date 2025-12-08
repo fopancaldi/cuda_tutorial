9bd983fff61453a1c0be85e1eb281ef726fcb9b5
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2025-12-08 18:07:50 +0100

    Add utility function

--- hpp/util.hpp
+++ hpp/util.hpp
@@ -9,6 +9,10 @@ auto ratio_rounded_up(std::integral auto num, std::integral auto den) {
     return (num + den - 1) / den;
 }
 
+auto round_up_to_multiple(std::integral auto to_round, std::integral auto multiple_base) {
+    return multiple_base * ratio_rounded_up(to_round, multiple_base);
+}
+
 template <typename T>
 std::size_t to_bytes(std::size_t count) {
     return count * sizeof(T);
