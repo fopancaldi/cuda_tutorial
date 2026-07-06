8048f8d92be653d36b556e0dca1aa6739f139b57
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2026-07-06 18:29:39 +0200

    Use templated range + forwarding reference

--- hpp/check.hpp
+++ hpp/check.hpp
@@ -5,6 +5,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <iterator>
 #include <ranges>
 #include <span>
 #include <utility>
@@ -35,35 +36,34 @@ bool are_equal(T const& t1, T const& t2) {
 } // namespace internal
 
 // TODO: Extend to ranges (to by taken by const& instead of value)
-template <std::ranges::view View1, std::ranges::view View2>
+template <std::ranges::range R1, std::ranges::range R2>
     requires requires {
-        typename std::common_type_t<std::ranges::range_value_t<View1>,
-                                    std::ranges::range_value_t<View2>>;
+        typename std::common_type_t<std::ranges::range_value_t<R1>, std::ranges::range_value_t<R2>>;
     }
-void check(View1 view1, View2 view2) {
-    assert(view1.size() == view2.size());
-    using Elem1 = std::ranges::range_value_t<View1>;
-    using Elem2 = std::ranges::range_value_t<View2>;
+void check(R1&& r1, R2&& r2) {
+    assert(std::size(r1) == std::size(r2));
+    using Elem1 = std::ranges::range_value_t<R1>;
+    using Elem2 = std::ranges::range_value_t<R2>;
     using Elem = std::common_type_t<Elem1, Elem2>;
 
     // TODO: In c++23, use std::ranges::all_of with std::views::zip
-    std::vector<Elem> equalities(view1.size());
-    std::ranges::transform(view1, view2, equalities.begin(), [](Elem1 const& e1, Elem2 const& e2) {
+    std::vector<Elem> equalities(std::size(r1));
+    std::ranges::transform(r1, r2, equalities.begin(), [](Elem1 const& e1, Elem2 const& e2) {
         return internal::are_equal(e1, e2);
     });
     assert(std::ranges::all_of(equalities, std::identity()));
 }
 
-template <std::ranges::view View, concepts::checker<std::ranges::range_value_t<View>> Checker>
-void check(View view, Checker&& checker) {
-    using Elem = std::ranges::range_value_t<View>;
+template <std::ranges::range R, concepts::checker<std::ranges::range_value_t<R>> Checker>
+void check(R&& r, Checker&& checker) {
+    using Elem = std::ranges::range_value_t<R>;
 
     // TODO: In c++23, use std::views::enumerate
-    std::vector<Elem> to_check(view.size());
+    std::vector<Elem> to_check(std::size(r));
     std::ranges::generate(to_check, [&c = std::as_const(checker), i = 0]() mutable {
         return static_cast<Elem>(c(i++));
     });
-    check(view, std::span(to_check));
+    check(r, std::span(to_check));
 }
 
 } // namespace cuda_tutorial
